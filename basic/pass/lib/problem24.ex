058f9874477d2be090cbe58e7cd0db7b6fbe0b4f
aethera <aethera.johnny@gmail.com>
2025-10-18 05:16:49 +0900

    solve24: 테스트 경고 존재함

--- basic/pass/lib/problem24.ex
+++ basic/pass/lib/problem24.ex
@@ -6,4 +6,11 @@ defmodule Problem24 do
   제수가 0인 경우 `{:error, :division_by_zero}` 튜플을 반환합니다.
   """
   # 여기에 safe_divide/2 함수를 구현하세요.
+  @spec safe_divide(number, number) :: {:ok, number} | {:error, :division_by_zero}
+
+  def safe_divide(_numerator, denominator)
+  when denominator == 0 or denominator == 0.0,
+  do: {:error, :division_by_zero}
+
+  def safe_divide(numerator, denominator), do: {:ok, numerator / denominator}
 end

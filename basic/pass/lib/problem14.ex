c6103cf6b7c97f1ec434565d80258777e1de6739
aethera <aethera.johnny@gmail.com>
2025-10-18 02:51:59 +0900

    solve14

--- basic/pass/lib/problem14.ex
+++ basic/pass/lib/problem14.ex
@@ -4,4 +4,8 @@ defmodule Problem14 do
   리스트가 비어있으면 `nil`을 반환합니다.
   """
   # 여기에 first_element/1 함수를 구현하세요.
+  @spec first_element(list(any)) :: any
+
+  def first_element([]), do: nil
+  def first_element([head | _]), do: head
 end

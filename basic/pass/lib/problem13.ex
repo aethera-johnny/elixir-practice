d3d463e9eb7835c737eec2f860dc6d5adccec6ea
aethera <aethera.johnny@gmail.com>
2025-10-18 02:46:15 +0900

    solve13

--- basic/pass/lib/problem13.ex
+++ basic/pass/lib/problem13.ex
@@ -3,4 +3,10 @@ defmodule Problem13 do
   주어진 문자열의 길이를 반환하는 함수를 구현합니다.
   """
   # 여기에 string_length/1 함수를 구현하세요.
+  @spec string_length(String.t()) :: non_neg_integer()
+
+  def string_length(message) do
+    String.length(message)
+  end
+
 end

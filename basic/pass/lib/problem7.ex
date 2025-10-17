b99083b5b02d2a381b79b525cf8ed32a0711e147
aethera <aethera.johnny@gmail.com>
2025-10-18 00:50:07 +0900

    solve7

--- basic/pass/lib/problem7.ex
+++ basic/pass/lib/problem7.ex
@@ -3,4 +3,7 @@ defmodule Problem7 do
   첫 번째 숫자가 두 번째 숫자보다 큰지 확인하는 함수를 구현합니다.
   """
   # 여기에 is_greater/2 함수를 구현하세요.
+  @spec is_greater(number, number) :: boolean
+
+  def is_greater(a, b), do: a > b
 end

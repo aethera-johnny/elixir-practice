c3c869f7bd71e6da6a9003b470b8e8f3a8c6b95a
aethera <aethera.johnny@gmail.com>
2025-10-18 03:02:06 +0900

    solve16

--- basic/pass/lib/problem16.ex
+++ basic/pass/lib/problem16.ex
@@ -3,4 +3,7 @@ defmodule Problem16 do
   주어진 리스트의 요소 수를 반환하는 함수를 구현합니다.
   """
   # 여기에 list_size/1 함수를 구현하세요.
+  @spec list_size(list(any)) :: non_neg_integer()
+
+  def list_size(list), do: Enum.count(list)
 end

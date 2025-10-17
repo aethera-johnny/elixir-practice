7134e9a5a00b148919a79f1ac1e73a9c8764a517
aethera <aethera.johnny@gmail.com>
2025-10-18 02:34:04 +0900

    solve11

--- basic/lib/problem11.ex
+++ basic/lib/problem11.ex
@@ -3,4 +3,9 @@ defmodule Problem11 do
   이름을 인자로 받아 "Hello, [이름]!" 형식의 환영 메시지를 반환하는 함수를 구현합니다.
   """
   # 여기에 greet/1 함수를 구현하세요.
+  @spec greet(String.t()) :: String.t()
+
+  def greet(userName) do
+    "Hello, #{userName}!"
+  end
 end

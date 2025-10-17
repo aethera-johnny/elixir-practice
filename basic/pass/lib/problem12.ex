a26e8580001baba081178adfae16ae95c28c960c
aethera <aethera.johnny@gmail.com>
2025-10-18 02:41:06 +0900

    solve12

--- basic/pass/lib/problem12.ex
+++ basic/pass/lib/problem12.ex
@@ -3,4 +3,7 @@ defmodule Problem12 do
   두 개의 문자열을 연결하는 함수를 구현합니다.
   """
   # 여기에 concatenate/2 함수를 구현하세요.
+  @spec concatenate(String.t(), String.t()) :: String.t()
+
+  def concatenate(a, b), do: a <> b
 end
